2356e2ac422b84bf2fd6207e75db19dd52b54c0c
Matteo Harchi <matteo.harchi@gmail.com>
2020-09-10 10:15:40 +0200

    New ed edit con invio + commenti

--- js/script.js
+++ js/script.js
@@ -1,28 +1,50 @@
 $(document).ready(function(){
+    //faccio partire la funzione che mi inserisce le voci
     getData();
+    //premo tasto enter o invio e creo nuova voce
     $("#enter").click(function(){
         var newNote = $(".newnote").val();
         createElement(newNote);
     });
-
+    $(document).on('keydown', ".newnote", function(){
+        if (event.which == 13 || event.keyCode == 13) {
+            var newNote = $(".newnote").val();
+            createElement(newNote);
+        }
+    })
+//al click del trash cancella la nota corrispondente
     $(document).on('click', 'i.fa-trash', function(){
         var elemento = $(this);
         var idtoDo = elemento.parent().attr('data-id');
         deleteElement(idtoDo);
     });
+// quando clicco su una voce questa diventa una barra di input+tasto
     $(document).on('click', '#note', function(){
         var elemento = $(this);
         var idtoDo = elemento.parent().attr('data-id');
         var edit ='<input class="editnote" type="text" name="" value="">' + '<button id="edit" type="button" name="button">Edit</button>';
         elemento.replaceWith(edit);
     });
+// quando clicco su edit o premo invio modifica la voce con contenuto input
     $(document).on('click', '#edit', function(){
         var edit = $(this);
         var newNote = $(".editnote").val();
         var idtoDo = edit.parent().attr('data-id');
-        edit.replaceWith('<span id="note">{{text}}</span>')
+        edit.replaceWith('<span id="note">{{text}}</span>');
         updateElement(newNote, idtoDo, edit);
     });
+
+    $(document).on('keydown', '.editnote', function(){
+        if (event.which == 13 || event.keyCode == 13) {
+            var edit = $(".editnote");
+            var newNote = $(".editnote").val();
+            var idtoDo = edit.parent().attr('data-id');
+            edit.replaceWith('<span id="note">{{text}}</span>');
+            updateElement(newNote, idtoDo, edit);
+        };
+    });
+//si sarebbe potuto fare anche con una classe hidden da dare prima
+//alla voce e poi alla barra di input
 });
 
 
@@ -60,8 +82,7 @@ function createElement(input){
             },
             success: function(risposta){
                 var newOne = risposta.text;
-                $("#list").empty();
-                getData();
+                replace();
             }
         })
 }
@@ -72,8 +93,7 @@ function deleteElement(id){
             url: "http://157.230.17.132:3016/todos/"+id,
             method: "DELETE",
             success: function(risposta){
-                $("#list").empty();
-                getData();
+                replace();
             }
         }
     )
@@ -90,11 +110,14 @@ function updateElement(edited, id, bar){
                 text: edited,
             },
             success: function(risposta){
-
-
-                $("#list").empty();
-                getData();
+                replace();
             },
         }
     )
+}
+
+//refresh della lista
+function replace(){
+    $("#list").empty();
+    getData();
 }
