35c8755edf8fb6010588f38d861c1b090a6fd452
Matteo Harchi <matteo.harchi@gmail.com>
2020-09-10 09:35:15 +0200

    fatto anche patch

--- js/script.js
+++ js/script.js
@@ -3,15 +3,32 @@ $(document).ready(function(){
     $("#enter").click(function(){
         var newNote = $(".newnote").val();
         createElement(newNote);
-    })
+    });
 
     $(document).on('click', 'i.fa-trash', function(){
         var elemento = $(this);
         var idtoDo = elemento.parent().attr('data-id');
         deleteElement(idtoDo);
-    })
+    });
+    $(document).on('click', '#note', function(){
+        var elemento = $(this);
+        var idtoDo = elemento.parent().attr('data-id');
+        var edit ='<input class="editnote" type="text" name="" value="">' + '<button id="edit" type="button" name="button">Edit</button>';
+        elemento.replaceWith(edit);
+    });
+    $(document).on('click', '#edit', function(){
+        var edit = $(this);
+        var newNote = $(".editnote").val();
+        var idtoDo = edit.parent().attr('data-id');
+        edit.replaceWith('<span id="note">{{text}}</span>')
+        updateElement(newNote, idtoDo, edit);
+    });
 });
 
+
+//FUNZIONI
+
+//funzione che esegue chiamata ajax e stampa todolist
 function getData(){
     var source = $("#template").html();
     var template = Handlebars.compile(source);
@@ -32,7 +49,7 @@ function getData(){
         }
     )
 };
-
+//funzione che crea la nuova voce nella todolist e refresha la lista
 function createElement(input){
     $.ajax(
         {
@@ -48,7 +65,7 @@ function createElement(input){
             }
         })
 }
-
+//funzione che cancella l'elemento selezionato e refresha la todolist
 function deleteElement(id){
     $.ajax(
         {
@@ -61,16 +78,23 @@ function deleteElement(id){
         }
     )
 }
-// function updateAndPrint(input){
 
-//     $.ajax(
-//         {
-//             url: "http://157.230.17.132:3016/todos",
-//             method: "UPDATE",
-//             data: {
-//                 text: input,
-//             },success: function(){
+//funzione modifica nota
 
-//             }
-//     })
-// }
+function updateElement(edited, id, bar){
+    $.ajax(
+        {
+            url: "http://157.230.17.132:3016/todos/"+id,
+            method: "PATCH",
+            data: {
+                text: edited,
+            },
+            success: function(risposta){
+
+
+                $("#list").empty();
+                getData();
+            },
+        }
+    )
+}
